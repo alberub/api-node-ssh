ceb5c1f3044cf87d0bcb3cf07e6ffcd562565a60
Alberto Rios <rios.alb2606@gmail.com>
2024-09-23 14:12:27 -0600

    Se forza un error al usar ==

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const port = 3000;
 
 // Ruta raíz
 app.get('/', (req, res) => {
-    res.send('Despliegue de api via SSH');
+    const cadena1 = 'Hola mundo';
+    const cadena2 = 'hola mundo';
+    let mensaje = '';
+    if (cadena1 == cadena2) {
+        mensaje = 'Las cadenas son iguales';
+    }
+    res.send(`Despliegue de api en servidor via SSH: ${mensaje}`);
 });
 
 app.listen(port, () => {
