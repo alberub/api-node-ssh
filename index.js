08b5060c6e70b7c0fc882dc3796c52d4e8179f00
Alberto Rios <rios.alb2606@gmail.com>
2024-09-25 21:38:51 -0600

    Se elimina el código comentado para que pase el análisis de sonar.

--- index.js
+++ index.js
@@ -10,28 +10,6 @@ app.get('/', (req, res) => {
     res.send(funcionDuplicada());
 });
 
-// app.get('/', (req, res) => {
-//     const cadena1 = 'Hola mundo';
-//     const cadena2 = 'hola mundo';
-//     const arr = [1,2,3,4,5];
-//     let nuevoMensaje = '';
-//     let mensaje = '';
-//     if (cadena1 != cadena2) {
-//         mensaje = 'Las cadenas son iguales';
-//         if (arr.some(1)) {            
-//             arr.forEach( el => {
-//                 if (el === 3) {
-//                     nuevoMensaje = "El número 3 se encuentra en el array";
-//                     if (nuevoMensaje.trim().length > 0) {
-//                         nuevoMensaje = "Entró en la segunda condición";
-//                     }                
-//                 }
-//             })
-//         }
-//     }
-//     res.send(funcionDuplicada());
-// });
-
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
