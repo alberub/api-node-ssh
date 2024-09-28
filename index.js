118b86bf64ded05fb5685ab4bf0abb7c780387de
Alberto Rios <rios.alb2606@gmail.com>
2024-09-28 15:30:59 -0600

    Pipeline llamando a otro

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 function funcionDuplicada() {
-    return 'Primer commit con un pipeline llamando a otro.';
+    return 'Primer commit con un pipeline llamando a otro';
 }
 
 app.get('/', (req, res) => {
